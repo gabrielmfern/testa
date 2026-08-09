f8ac0bd6c9579c3e074a1bb15dac943f52bfd4e9
Gabriel Miranda <gabrielmfern@outlook.com>
2026-08-09 14:33:57 -0300

    custom timeout values
    Signed-off-by: Gabriel Miranda <gabrielmfern@outlook.com>

--- tests/hang.test.ts
+++ tests/hang.test.ts
@@ -2,6 +2,7 @@ test("hang with async work", async () => {
   function wait(time: number) {
     return new Promise<void>((resolve) => {
       setTimeout(() => {
+        console.log('this will still fire because we can\'t clear timeouts');
         resolve();
       }, time);
     });
@@ -10,7 +11,22 @@ test("hang with async work", async () => {
 });
 
 test("hang with sync work", () => {
-  while(true) {
-    // idiot loop
-  }
+  while(true) { }
 });
+
+test("hang with async work in 100ms", async () => {
+  function wait(time: number) {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        console.log('this will still fire because we can\'t clear timeouts');
+        resolve();
+      }, time);
+    });
+  }
+  await wait(200);
+}, 100);
+
+test("hang with sync work in 100ms", () => {
+  while(true) { }
+}, 100);
+
