1d8c258cb680b0b37276f2b08af004b546f01e9a
gabriel miranda <gabrielmfern@outlook.com>
2026-08-15 16:28:21 -0300

    add error throwing for failing tests
    Signed-off-by: gabriel miranda <gabrielmfern@outlook.com>

--- tests/failing.spec.ts
+++ tests/failing.spec.ts
@@ -1,3 +1,11 @@
 test.failing("failing test", () => {
   expect(2).toBe(0);
 });
+
+test.failing("throwing an Error", () => {
+  throw new Error("this is my error, this test should fail");
+});
+
+test.failing("throwing an Error async", async () => {
+  throw new Error("this is my error, this test should fail");
+});
