894b38a632da8eefd0291e058407a1d9d7b1ec92
gabriel miranda <gabrielmfern@outlook.com>
2026-08-19 11:58:28 -0300

    fix windows build

--- shim.h
+++ shim.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "node_api_types.h"
 
+#ifdef _WIN32
+#define SHIM_EXPORT __declspec(dllexport)
+#else
+#define SHIM_EXPORT
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -11,37 +17,37 @@ typedef struct node_common_environment_setup node_common_environment_setup;
 typedef struct node_environment node_environment;
 typedef struct v8_scope v8_scope;
 
-node_initialization_result* node_initialize_once_per_process(void);
-int node_initialization_result_early_return(node_initialization_result* result);
-int node_initialization_result_exit_code(node_initialization_result* result);
-void node_tear_down_once_per_process(void);
+SHIM_EXPORT node_initialization_result* node_initialize_once_per_process(void);
+SHIM_EXPORT int node_initialization_result_early_return(node_initialization_result* result);
+SHIM_EXPORT int node_initialization_result_exit_code(node_initialization_result* result);
+SHIM_EXPORT void node_tear_down_once_per_process(void);
 
-node_multi_isolate_platform* node_multi_isolate_platform_create(int thread_pool_size);
+SHIM_EXPORT node_multi_isolate_platform* node_multi_isolate_platform_create(int thread_pool_size);
 
-void v8_initialize(node_multi_isolate_platform* platform);
-void v8_dispose(node_multi_isolate_platform* platform);
+SHIM_EXPORT void v8_initialize(node_multi_isolate_platform* platform);
+SHIM_EXPORT void v8_dispose(node_multi_isolate_platform* platform);
 
-node_common_environment_setup* node_common_environment_setup_create(node_multi_isolate_platform* platform, node_initialization_result* result);
-node_environment* node_common_environment_setup_env(node_common_environment_setup* setup);
-void node_common_environment_setup_destroy(node_common_environment_setup* setup);
+SHIM_EXPORT node_common_environment_setup* node_common_environment_setup_create(node_multi_isolate_platform* platform, node_initialization_result* result);
+SHIM_EXPORT node_environment* node_common_environment_setup_env(node_common_environment_setup* setup);
+SHIM_EXPORT void node_common_environment_setup_destroy(node_common_environment_setup* setup);
 
-v8_scope* v8_scope_open(node_common_environment_setup* setup);
-void v8_scope_close(v8_scope* scope);
+SHIM_EXPORT v8_scope* v8_scope_open(node_common_environment_setup* setup);
+SHIM_EXPORT void v8_scope_close(v8_scope* scope);
 
-void node_add_linked_binding(node_environment* env, const char* name, napi_addon_register_func fn);
-int node_load_environment(node_environment* env, const char* script);
-int node_load_environment_module(node_environment* env, const char* source, const char* resource_name);
-int node_spin_event_loop(node_environment* env);
-int node_stop(node_environment* env);
-int node_spin_event_loop_once(node_common_environment_setup* setup);
-void node_stop_event_loop(node_common_environment_setup* setup);
-void node_cancel_terminate_execution(node_common_environment_setup* setup);
-void node_perform_microtask_checkpoint(node_common_environment_setup* setup);
+SHIM_EXPORT void node_add_linked_binding(node_environment* env, const char* name, napi_addon_register_func fn);
+SHIM_EXPORT int node_load_environment(node_environment* env, const char* script);
+SHIM_EXPORT int node_load_environment_module(node_environment* env, const char* source, const char* resource_name);
+SHIM_EXPORT int node_spin_event_loop(node_environment* env);
+SHIM_EXPORT int node_stop(node_environment* env);
+SHIM_EXPORT int node_spin_event_loop_once(node_common_environment_setup* setup);
+SHIM_EXPORT void node_stop_event_loop(node_common_environment_setup* setup);
+SHIM_EXPORT void node_cancel_terminate_execution(node_common_environment_setup* setup);
+SHIM_EXPORT void node_perform_microtask_checkpoint(node_common_environment_setup* setup);
 typedef struct v8_promise v8_promise;
-v8_promise* v8_promise_ref(napi_value value);
-int v8_promise_state(v8_promise* promise);
-napi_value v8_promise_result(v8_promise* promise);
-void v8_promise_unref(v8_promise* promise);
+SHIM_EXPORT v8_promise* v8_promise_ref(napi_value value);
+SHIM_EXPORT int v8_promise_state(v8_promise* promise);
+SHIM_EXPORT napi_value v8_promise_result(v8_promise* promise);
+SHIM_EXPORT void v8_promise_unref(v8_promise* promise);
 
 #ifdef __cplusplus
 }
